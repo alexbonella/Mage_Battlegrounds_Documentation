492623e29785dcb802cabe0ee313bd5385ab3a64
Alexander Bolaño <45697319+alexbonella@users.noreply.github.com>
2023-04-16 05:40:17 -0500

    Add project folders

---
+++ Ecommerce_Simulation/kafka_auth.py
@@ -0,0 +1,5 @@
+conf = {'bootstrap.servers': "pkc-n00kk.us-east-1.aws.confluent.cloud:9092",
+        'security.protocol':'SASL_SSL',
+        'sasl.mechanisms':'PLAIN',
+        'sasl.username' : 'xX_Xx_x_x_x_x_x',
+        'sasl.password' : 'xX_Xx_x_x_x_x_x'}
